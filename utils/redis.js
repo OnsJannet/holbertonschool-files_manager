c6382616ab04e1356487591d9e0253dd0598552d
onsjannet <onsjannet@gmail.com>
2021-10-06 13:52:26 +0100

    [Redis]

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+const Redis = require('redis')
+const { promisify } = require("util");
+
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on("error", function(error) {
+    console.log(`Redis client not connected to the server: ${error.toString()}`);
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+  
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+    async del(key) {
+    this.client.del(key);
+  }
+
+const redisClient = new RedisClient();
+export default redisClient;
